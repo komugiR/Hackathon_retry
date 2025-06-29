a6129810c329016eee1f8a3b0826184a1eadaba4
komugiR <kuroyukkuri38@gmail.com>
2025-06-29 11:16:16 +0900

    gamemanagerの追加

---
+++ Unity_project/Assets/Scripts/Gamemanager.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class GameManager : MonoBehaviour
+{
+    public RawImage imageUI;
+    public Text infoText;
+
+    [System.Serializable]
+    public class ImageData
+    {
+        public string image;
+        public string date;
+        public string day;
+        public string time;
+    }
+
+    public void OnReceiveImageData(string json)
+    {
+        Debug.Log("📦 受信データ: " + json);
+        ImageData data = JsonUtility.FromJson<ImageData>(json);
+
+        infoText.text = $"📅 {data.date}（{data.day}）\n🕒 {data.time}";
+        StartCoroutine(LoadImage(data.image));
+    }
+
+    IEnumerator LoadImage(string base64)
+    {
+        byte[] bytes = System.Convert.FromBase64String(base64.Replace("data:image/jpeg;base64,", "").Replace("data:image/png;base64,", ""));
+        Texture2D tex = new Texture2D(2, 2);
+        tex.LoadImage(bytes);
+        imageUI.texture = tex;
+        yield return null;
+    }
+}
